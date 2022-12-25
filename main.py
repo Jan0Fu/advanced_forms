4255d97a4300f141d95a739c23c58c40547ced75
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-25 17:10:35 +0100

    Fixed bugs, improvements

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-from wtforms import validators
+from wtforms.validators import DataRequired, Email, Length
 from flask_bootstrap import Bootstrap
-
-app = Flask(__name__)
-app.secret_key = "jj3sf#n5sf26gk@$gkgk45"
-Bootstrap(app)
 
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired(), validators.length(min=8), validators.Email()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
-    submit = SubmitField(label='Log In')
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label="Log In")
+
+
+app = Flask(__name__)
+app.secret_key = "hd2#kdsj34@#jfjf$kd"
+Bootstrap(app)
 
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -26,9 +26,10 @@ def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
         if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
-            return render_template('success.html')
-        return render_template('denied.html')
-    return render_template('login.html', form=login_form)
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=login_form)
 
 
 if __name__ == '__main__':
